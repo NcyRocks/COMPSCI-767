622fb5d919a5ad54e76dddf2cc8f8cafaf3e3f28
Nathan Young <nathan@nathanyoung.nz>
2021-04-22 15:21:23 +1200

    Quick ugly fix

--- assignment1/extra.py
+++ assignment1/extra.py
@@ -1,16 +1,16 @@
 import random
 
-import main
+import main as main_file
 
 
 def main():
     """Attempts to plot the relationship between strength of heuristic and compression ratio."""
     random.seed(42)  # I used 42 for the experiments
-    problem, states = main.a1_problems()
+    problem, states = main_file.a1_problems()
     puzzle_no = 8  # For the 8-puzzle, this is set to 8
     strengths = [8, 7, 6, 5, 4, 3, 2, 1]
 
-    main.get_pdbs("BIG_PDBS.txt", puzzle_no, strengths, problem)
+    main_file.get_pdbs("BIG_PDBS.txt", puzzle_no, strengths, problem)
 
 
 if __name__ == "__main__":
